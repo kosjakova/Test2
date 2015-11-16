738276f5134cf97a1ace1c39af97547028e3bd14
Victoria <viktorijabulvane@gmail.com>
2015-11-16 15:40:26 +0200

    Added variables x and y

--- T2/T2/Program.cs
+++ T2/T2/Program.cs
@@ -13,7 +13,27 @@ namespace T2
             
             Console.WriteLine("\n\n Press any key to exit!");
             
-            Console.ReadKey();
+            int x, y, counter = 0;
+
+            for (int i = 0; i == counter; i++)
+            {
+                try
+                {
+                    Console.Write("\n Enter please first number:  ");
+                    x = Int32.Parse(Console.ReadLine());
+
+                    Console.Write(" Enter please second number: ");
+                    y = Int32.Parse(Console.ReadLine());
+
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("\n " + error.Message);
+                    counter++;
+                }
+            }
+ 
+             Console.ReadKey();
         }
     }
 }
