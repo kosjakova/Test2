b05a7ce3741aacd013042784fb07e1ad0957773a
Victoria <viktorijabulvane@gmail.com>
2015-11-16 15:23:11 +0200

    Test 2

---
+++ T2/T2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n Test2");
+            
+            Console.WriteLine("\n\n Press any key to exit!");
+            
+            Console.ReadKey();
+        }
+    }
+}
